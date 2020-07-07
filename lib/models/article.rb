deccacd99e1699067c52fdc93cecbd5deb639c0a
Tony Kim <hjkmines@gmail.com>
2020-07-07 16:41:55 -0600

    Edit API KEY

--- lib/models/article.rb
+++ lib/models/article.rb
@@ -5,7 +5,7 @@ class Article < ActiveRecord::Base
   has_many :users, through: :feeds
   
   def self.output 
-  n = News.new("2a717d771d85444cb9cb8eda83414b88")
+  n = News.new("≈≈≈≈≈≈≈≈≈≈≈≈≈")
   n.get_sources(country: 'us', language: 'en')
   output = n.get_everything(q: "apple", from: "2020-07-01&to=2020-07-07", sortBy: "popularity")
   end 
