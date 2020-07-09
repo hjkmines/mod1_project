321ef85500696667af99037094d80ce339b6bfc6
Tony Kim <hjkmines@gmail.com>
2020-07-09 11:50:50 -0600

    Added more UI

--- lib/cli.rb
+++ lib/cli.rb
@@ -4,10 +4,10 @@ class Cli
     puts "Hello! What is your name?"
     $user_name = gets.strip  
     $user = User.create(name: $user_name)
-    choose_category
+    Cli.choose_category
   end 
   
-  def choose_category 
+  def self.choose_category 
     puts ""
     puts "Welcome #{$user_name}, what would you like to read about? Enter the number next to your chosen category."
     categories = ["1. Business", "2. Entertainment", "3. General", "4. Health", "5. Science", "6. Sports", "7. Technology", "8. Pick a Random Headline for Me", "9. View My Saved Articles"]
@@ -17,61 +17,71 @@ class Cli
       user_choice = "business"
       Article.categories(user_choice)
       puts ""
-      home_or_exit
+      Cli.home_or_exit
     elsif user_choice == 2 
       user_choice = "entertainment"
       Article.categories(user_choice)
       puts ""
-      home_or_exit
+      Cli.home_or_exit
     elsif user_choice == 3
       user_choice = "general"
       Article.categories(user_choice)
       puts ""
-      home_or_exit
+      Cli.home_or_exit
     elsif user_choice == 4
       user_choice = "health"
       Article.categories(user_choice)
       puts ""
-      home_or_exit
+      Cli.home_or_exit
     elsif user_choice == 5
       user_choice = "science"
       Article.categories(user_choice)
       puts ""
-      home_or_exit
+      Cli.home_or_exit
     elsif user_choice == 6
       user_choice = "sports"
       Article.categories(user_choice)
       puts ""
-      home_or_exit
+      Cli.home_or_exit
     elsif user_choice == 7
       user_choice = "technology"
       Article.categories(user_choice)
       puts ""
-      home_or_exit
+      Cli.home_or_exit
     elsif user_choice == 8 
       Article.random
       puts ""
-      home_or_exit
+      Cli.home_or_exit
     elsif user_choice == 9 
-      p $user.articles 
+      puts ""
+      puts "#{$user_name}, here are your saved articles:"
+      counter = 1
+      puts ""
+      $user.articles.each do |article|
+        puts ""
+        puts "#{counter}. #{article.title}" 
+        counter += 1
+      end    
+      puts ""
+      Cli.home_or_exit
     else 
       puts "Sorry, I didn't understand that. Please try again."
-      choose_category
+      Cli.choose_category
     end 
 
   end
 
-  def home_or_exit 
-    puts "Do you wish to go back to the home page or exit the program? Enter (Yes or Exit)"
+  def self.home_or_exit 
+    puts "Do you wish to go back to the home page or exit the program? Enter (Home or Exit)"
     user_response = gets.strip 
-    if user_response == "Y" || user_response == "y" || user_response == "Yes" || user_response == "yes"
-      choose_category
+    if user_response == "H" || user_response == "h" || user_response == "Home" || user_response == "home"
+      Cli.choose_category
     elsif 
       user_response == "exit" || user_response == "Exit"
       exit 
     else
       puts "Sorry, I didn't understand that. Please try again."
-      home_or_exit
+      Cli.home_or_exit
     end 
   end 
 
--- lib/models/article.rb
+++ lib/models/article.rb
@@ -14,47 +14,85 @@ class Article < ActiveRecord::Base
   end 
 
   def self.top_five(stories, category)
-  puts ""
-  puts "Here are today's top 5 #{category} stories:"
-    counter = 1 
-    result = stories["articles"].take(5).each do |key, value|
-      puts "#{counter}. " + "#{key["title"]}"
-      counter += 1
-    end  
-  puts "If you would like to read more, enter the number next to your chosen headline."
-  user_selected_input = gets.strip.to_i 
-  puts ""
-    result.each do |article| 
-        if result.index(article) == (user_selected_input - 1)
-          puts article["title"]
-          puts ""
-          puts article["description"]
-          puts ""
-          puts article["url"]
-          puts ""
-          puts "Want to save this story? Enter (Yes/No)"
-            user_response = gets.strip 
-              if user_response == "Y" || user_response == "y" || user_response == "Yes" || user_response == "yes"
-                article = Article.create(title: article["title"], summary: article["description"], category: category, time_stamp: Time.now)
-                Feed.create(user_id: $user.id, article_id: article.id)
-                puts ""
-                puts "Article has been saved!"
-              else 
-                exit 
-              end 
-          return 
-        else 
-          puts "Sorry, I didn't understand that. Please try again."
-        end
+    puts ""
+    puts "Here are today's top 5 #{category} stories:"
+      counter = 1 
+      result = stories["articles"].take(5).each do |key, value|
+        puts "#{counter}. " + "#{key["title"]}"
+        counter += 1
+      end  
+    puts ""
+    # puts "If you would like to read more, enter the number next to your chosen headline."
+    puts "Type the number next to the headline you would like to read more about, or type '0' to go back."
+    
+    user_selected_input = gets.strip.to_i 
+    puts ""
+    if user_selected_input == 0
+      Cli.choose_category
+    else 
+      result.each do |article| 
+          if result.index(article) == (user_selected_input - 1)
+            puts article["title"]
+            puts ""
+            puts article["description"]
+            puts ""
+            puts article["url"]
+            puts ""
+            puts "Want to save this story? Enter (Yes/No)"
+              user_response = gets.strip 
+                if user_response == "Y" || user_response == "y" || user_response == "Yes" || user_response == "yes"
+                  article = Article.create(title: article["title"], summary: article["description"], category: category, time_stamp: Time.now)
+                  $user.articles << article 
+                  puts ""
+                  puts "Article has been saved!"
+                else 
+                  Cli.home_or_exit
+                end 
+          end 
+        end 
       end 
-  end 
+      # user_selected_input = gets.strip
+      # if user_selected_input == "home"
+      #     Cli.choose_category
+      # end
+        
+      # user_selected_input = gets.strip.to_i
+      if user_selected_input != (1..5)
+        puts "Sorry, I didn't understand that. Please try again."
+        Article.categories(category)
+      end
+  
+        Cli.home_or_exit
+    end 
 
   def self.random 
   key = ENV["API_KEY"]
   response = RestClient.get ("https://newsapi.org/v2/top-headlines?country=us&apiKey=" + "#{key}")
   parsed_response = JSON.parse(response)
   puts parsed_response["articles"].sample["title"]
+  puts ""
+  puts "If you would like to read more, type 'more'."
+  user_selected_input = gets.strip.to_s
+  if user_selected_input == "more" || user_selected_input == "More"
+    puts parsed_response["articles"].sample["title"]
+    puts parsed_response["articles"].sample["description"]
+    puts parsed_response["articles"].sample["url"]
+  end
+  puts ""
+  Cli.home_or_exit
+  # puts ""
+  # puts "Want to save this story? Enter (Yes/No)"
+  # user_response = gets.strip 
+  #   if user_response == "Y" || user_response == "y" || user_response == "Yes" || user_response == "yes"
+  #     article = Article.create(title: article["title"], summary: article["description"], category: category, time_stamp: Time.now)
+  #     $user.articles << article
+  #     # Feed.create(user_id: $user.id, article_id: article.id)
+  #     puts ""
+  #     puts "Article has been saved!"
+  #   else 
+  #     exit 
+  #   end 
+  # end 
   end 
-
 end 
 
